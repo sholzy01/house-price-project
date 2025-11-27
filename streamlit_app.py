59ef93682a06dc02258bbdbfca0f4737abf4cb6b
sholzy01 <158043054+sholzy01@users.noreply.github.com>
2025-11-27 14:49:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 import joblib
-joblib.dump(model, "rf_model.joblib")
-
 st.title('California House Price Predictor (Demo)')
 
 # Load model (the notebook will save rf_model.joblib next to this file)
